01f4634a6e737c41cc14462cc131c8de7b66deaf
Brandon Young <robotgenis@gmail.com>
2022-01-25 14:41:27 -0500

    test command runner with timeout

---
+++ runner.py
@@ -0,0 +1,6 @@
+import time
+
+
+for i in range(10):
+    time.sleep(0.5)
+    print("Run Test: ", i)
--- test.py
+++ test.py
@@ -1,14 +1,36 @@
-import subprocess
-import os
+from subprocess import Popen, PIPE
+from time import sleep, time
+import threading
 
-processes = []
-for file in files_output:
-    f = os.tmpfile()
-    p = subprocess.Popen(['md5sum',file],stdout=f)
-    processes.append((p, f))
+def runCommandThread(cmd_list:list):
+	p = Popen(cmd_list, stdout=PIPE, stderr=PIPE)
+	stdout = p.stdout.read()
+	stderr = p.stderr.read()
+	if stdout:
+		print(stdout)
+	if stderr:
+		print(stderr)
 
-for p, f in processes:
-    p.wait()
-    f.seek(0)
-    logfile.write(f.read())
-    f.close()
+
+def waitThread(t:float):
+    sleep(t)
+
+def runCommandWithTimout(cmd_list:list, t:float):
+	threadCommand = threading.Thread(target=runCommandThread, args=(cmd_list,))
+	threadCommand.daemon = True
+	threadTimer = threading.Thread(target=waitThread, args=(timers,))
+
+	threadCommand.start()
+	threadTimer.start()
+
+	threadTimer.join()
+	if(threadCommand.is_alive()):
+		print("Out of time")
+
+	print("Done")
+
+
+cmd_list = ['python runner.py']
+timers = 6.0
+
+runCommandWithTimout(cmd_list, timers)
