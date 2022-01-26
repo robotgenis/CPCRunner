979317c57f8ae84209ca37accae065af5bea5dcd
Brandon Young <robotgenis@gmail.com>
2022-01-25 20:35:04 -0500

    backup

--- myThreader.py
+++ myThreader.py
@@ -3,16 +3,16 @@ from time import sleep, time
 import threading
 import sys
 
-def runCommandThread(cmd_list:list):
-	p = Popen(cmd_list, stdout=PIPE, stderr=PIPE, stdin=sys.stdin, shell=True) #
+def runCommandThread(cmd_list:list, folder:str):
+	p = Popen(cmd_list, stdout=PIPE, stderr=PIPE, stdin=sys.stdin, shell=True, cwd=folder) #
 	st = time()
 	stdout = p.stdout.read()
 	stderr = p.stderr.read()
 	if stdout:
-		print("t:", time() - st)
-		print("o:",str(stdout))
+		print("t:" +  str(time() - st))
+		print("o:" + str(stdout.decode('ascii').split("\r\n")))
 	if stderr:
-		print("e:",str(stderr))
+		print("e:" + str(stderr))
 	global commandComplete
 	commandComplete = True
 
@@ -21,14 +21,14 @@ def waitThread(t:float):
 	global timerComplete
 	timerComplete = True
 
-def runCommandWithTimout(cmd_list:list, t:float):
+def runCommandWithTimout(cmd_list:list, t:float, folder:str):
 	print("l:starting")
 	global commandComplete
 	global timerComplete
 	commandComplete = False
 	timerComplete = False
 	
-	threadCommand = threading.Thread(target=runCommandThread, args=(cmd_list,))
+	threadCommand = threading.Thread(target=runCommandThread, args=(cmd_list, folder))
 	threadCommand.daemon = True
 	threadTimer = threading.Thread(target=waitThread, args=(t,))
 	threadTimer.daemon = True
@@ -52,12 +52,18 @@ for i in sys.argv:
 		timers = float(arr[1])
 		break
 
-
+folder = ""
 for i in sys.argv:
 	arr = i.split("=")
 	if arr[0] == "f" and len(arr) >= 2:
+		folder = str(arr[1])
+		break
+
+for i in sys.argv:
+	arr = i.split("=")
+	if arr[0] == "c" and len(arr) >= 2:
 		f = str(arr[1])
-		runCommandWithTimout(f, timers)
+		runCommandWithTimout(f, timers, folder)
 		break
 
 #runCommandWithTimout("python C:\\Users\\young\\Documents\\Code\\CPC\\Runner\\CPCRunner\\runner.py", 6.0)
--- runProgram.py
+++ runProgram.py
@@ -1,5 +1,6 @@
 
 from fileinput import filename
+from re import L
 from subprocess import Popen, PIPE
 import os
 from time import time
@@ -12,6 +13,11 @@ progFolder = os.path.dirname(__file__)
 
 TYPE_PYTHON = "TYPE_PYTHON"
 TYPE_JAVA = "TYPE_JAVA"
+TYPE_CPP = "TYPE_CPP"
+
+OUTPUT_ERROR = "OUTPUT_ERROR"
+OUTPUT_TIMEOUT = "OUTPUT_TIMEOUT"
+OUTPUT_SUCCESS = "OUTPUT_SUCCESS"
 
 def saveProgram(fileName:str, prog:list):
     f = open(os.path.join(progFolder, fileName), "x")
@@ -22,7 +28,7 @@ def saveProgram(fileName:str, prog:list):
 
 def runProgram(typ:str, tim:float, prog:str, inps:list):
 
-    runFolder = str(time()).replace(".","-") + "-" + str(random.random())[2:] + "/"
+    runFolder = "F" + str(time()).replace(".","_") + "_" + str(random.random())[2:] + "\\"
     
     runFolderPath = os.path.join(progFolder, runFolder)
     
@@ -30,43 +36,80 @@ def runProgram(typ:str, tim:float, prog:str, inps:list):
 
     cmd = ""
     if typ == TYPE_PYTHON:
-        fileName = runFolder + "test.py"
-        saveProgram(fileName, prog)
+        fileName = "test.py"
+        saveProgram(runFolder + fileName, prog)
         cmd = "python " + fileName
     elif typ == TYPE_JAVA:
-        
-        arrProg = " ".join(prog).split()
-        
+        arrProg = prog.split()
         fileName = ""
-        
         for i in range(len(arrProg) - 2):
             if arrProg[i] == "public" and arrProg[i+1] == "class":
-                fileName = runFolder + arrProg[i+2]
+                fileName = arrProg[i+2].split("{")[0]
                 break
             elif arrProg[i+1] == "class" and len(fileName) == 0:
-                fileName = runFolder + arrProg[i+1]
-        print(fileName)
-        p = Popen("javac " + fileName + ".java", stdout=PIPE, stderr=PIPE, stdin=PIPE)
+                fileName = arrProg[i+1].split("{")[0]
+        saveProgram(runFolder + fileName + ".java", prog)
+        p = Popen("javac " + runFolder + fileName + ".java", stdout=PIPE, stderr=PIPE, stdin=PIPE)
         p.wait()
         cmd = "java " + fileName
+    elif typ == TYPE_CPP:
+        fileName = "test.cpp"
+        saveProgram(runFolder + fileName, prog)
+        p = Popen("g++ -o ./" + runFolder + "test.exe " + runFolder + fileName, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        p.wait()
+        cmd = ".\\test.exe"
+        
+        
 
     #Python Exmaple
     # cmd = "python runs/runner.py"
     # inp = "test\n1\n2\n3".encode("ascii")
 
+    testRuns = []
 
+    for i in inps:
+        runCmd = "python myThreader.py t=" + str(tim) + " \"c=" + cmd + "\" f=" + runFolderPath + ""
+        p = Popen(runCmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
-    runCmd = "python myThreader.py t=" + str(tim) + " \"f=" + cmd + "\""
-    p = Popen(runCmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        # python test.py t=6.0 "f=python C:\Users\young\Documents\Code\CPC\Runner\CPCRunner\runner.py"
+        stdout, stderr = p.communicate(i.encode("ascii"))
 
-    # python test.py t=6.0 "f=python C:\Users\young\Documents\Code\CPC\Runner\CPCRunner\runner.py"
-    stdout, stderr = p.communicate("\n".encode("ascii"))
+        if stdout:
+            out = stdout.decode('ascii').split("\r\n")
+            
+            output = ""
+            timer = 0.0
+            error = ""
+            for i in out:
+                if len(i) > 0 and i[0] == "t":
+                    timer = float(i[2:])
+                elif len(i) > 0 and i[0] == "o":
+                    output = eval(i[2:])
+                elif len(i) > 0 and i[0] == "e":
+                    error = i[2:]
+            testRuns.append((i,timer,output, error))
 
-    if stdout:
-        print("o:",str(stdout).split("\\r\\n"))
-    if stderr:
-        print("e:",str(stderr))
-    
+        if stderr:
+            print("e:",str(stderr))
+            return (OUTPUT_ERROR, str(stderr))
+    print(testRuns)       
+        
     shutil.rmtree(runFolderPath)
 
-runProgram(TYPE_PYTHON, 6.0, "print('hi')", [])
+runProgram(TYPE_PYTHON, 6.0, """
+print('[\\n',input(),'\\n]')
+""", ["a", "hi there", "your mom"])
+# runProgram(TYPE_JAVA,6.0, """public class Hello{
+# 	public static void main(String[] args){
+# 		System.out.println("hi");
+# 	}
+# }""", [])
+# runProgram(TYPE_CPP, 6.0, """
+# #include <iostream>
+
+# using namespace std;
+# int main() {
+#     cout << "Hello World!";
+#     return 0;
+# }             
+# """, [])
