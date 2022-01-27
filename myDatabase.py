8c44f56273a7a797c7d6734feb29647d47052c76
Brandon Young <robotgenis@gmail.com>
2022-01-27 17:09:02 -0500

    database start not tested

--- myDatabase.py
+++ myDatabase.py
@@ -1,12 +1,39 @@
-
-
 import json
 
+database = {'problems':[{'name':'Problem A'}]}
 
 
-def loadFile(fil:str):
-    pass
+def loadFileJson(fil:str):
+	try:
+		f = open(fil, "r")
+		lines = f.readlines()
+		f.close()
+		return json.loads("".join(lines))
+	except:
+		return
 
+def saveFileJson(fil:str, j):
+	try:
+		f = open(fil, "w")
+		f.write(json.dumps(j))
+		f.close()
+	except:
+		return
 
-def saveFile(fil:str):
-    pass
+def saveDatabaseSection(name):
+	if name in database:
+		saveFileJson(name + ".json", database[name])
+
+def loadDatabaseSection(name):
+	database[name] = loadFileJson(name + ".json")
+
+loadDatabaseSection('problems')
+loadDatabaseSection('test')
+
+a = database['problems']
+
+database['problems'][0]['details'] = "something"
+
+print(database)
+
+saveDatabaseSection('problems')
